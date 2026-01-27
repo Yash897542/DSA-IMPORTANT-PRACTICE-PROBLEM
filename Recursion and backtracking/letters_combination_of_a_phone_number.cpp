8c4183cdae31a0cb24e911014c28217683c0b75e
Yash897542 <vyash917@gmail.com>
2026-01-27 17:53:51 +0530

    updated changes

---
+++ Recursion and backtracking/letters_combination_of_a_phone_number.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<string>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<string>result;
+    void solve(int idx , string &digits , string &temp , unordered_map<char,string>&mp){
+        if(idx >= digits.length()){
+            result.push_back(temp);
+            return;
+        }
+        char ch = digits[idx];
+        string str = mp[ch];
+        for(int i = 0 ; i<str.length() ; i++){
+            temp.push_back(str[i]);
+            solve(idx + 1 , digits , temp , mp);
+            temp.pop_back();
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0){
+            return {};
+        }
+        
+        unordered_map<char,string>mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+
+        string temp = "";
+        
+        solve(0 , digits , temp , mp);
+        return result;
+
+
+        
+    }
+};int main(){
+    string digits;
+    cin>> digits;
+    Solution obj;
+    vector<string>ans = obj.letterCombinations(digits);
+    for(auto &str : ans){
+        cout<< str << " ";
+    }
+    cout<< endl;
+    return 0;
+}
