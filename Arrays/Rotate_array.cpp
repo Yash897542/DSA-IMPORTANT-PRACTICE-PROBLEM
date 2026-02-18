cdbf22ddc3e756b473a4efe9e5e328ae45208ef6
Yash897542 <vyash917@gmail.com>
2026-02-18 21:29:18 +0530

    updated changes

---
+++ Arrays/Rotate_array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        if(n == 0){
+            return ;
+        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin() , nums.begin() + k);
+        reverse(nums.begin() + k , nums.end());
+    }
+};
+int main(){
+    Solution s;
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    int k = 3;
+    s.rotate(nums , k);
+    for(int i : nums){
+        cout << i << " ";
+    }
+    cout << endl; // Output: [5, 6, 7, 1, 2, 3, 4]
+    return 0;
+}
